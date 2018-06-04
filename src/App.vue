55db63580d72052d140b59cbdbf40d69a77225ac
Joscha Holzhäuer <jh@dreiqbik.de>
2018-06-04 17:29:36 +0200

    project setup

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Block
+      v-for="block in numberOfBlocks"
+      :key="block.id"
+    />
+  </div>
+</template>
+
+<script>
+import Block from './components/Block';
+
+export default {
+  name: 'App',
+  components: {
+    Block
+  },
+  data() {
+    return {
+      numberOfBlocks: 2
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+
+body {
+  margin: 0;
+  background-color: #F2F4F6;
+}
+
+#app {
+  font-family: "Open Sans", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #F2F4F6;
+  padding: 2em;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 1em;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="block">
+    <h2>{{ blockHeading }}</h2>
+    <form v-on:submit="addItem">
+      <input
+        type="text"
+  			v-model="newItem.name"
+  			placeholder="Name"
+  		/>
+      <input
+        type="text"
+  			v-model="newItem.link"
+  			placeholder="Link"
+  		/>
+      <input type="submit" value="Link hinzufügen">
+    </form>
+    <ul v-if="blockItems.length">
+      <li
+        v-for="item in blockItems"
+        :key="item.id"
+        >
+        <a :href="item.link">{{ item.name }}</a>
+      </li>
+    </ul>
+    <p v-else>No links entered yet</p>
+  </div>
+</template>
+
+<script>
+let nextItemId = 1;
+
+export default {
+
+  name: "Block",
+  data() {
+    return {
+      blockHeading: "Links",
+      newItem: {},
+      blockItems: [
+        {
+          id: nextItemId++,
+          name: "Medium",
+          link: "https://medium.com"
+        },
+        {
+          id: nextItemId++,
+          name: "Die Zeit",
+          link: "https://zeit.de"
+        }
+      ]
+    };
+  },
+  methods: {
+    addItem: function(e) {
+      e.preventDefault();
+
+      if (this.newItem.name && this.newItem.link) {
+
+        const trimmedName = this.newItem.name.trim();
+        const trimmedLink = this.newItem.link.trim();
+
+        this.blockItems.push({
+          id: nextItemId++,
+          name: trimmedName,
+          link: trimmedLink
+        });
+        this.newItem = {};
+      }
+    },
+    // removeItem(idToRemove) {
+    //   this.todos = this.todos.filter(todo => {
+    //     return todo.id !== idToRemove;
+    //   });
+    // }
+  }
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1,
+h2 {
+  font-weight: 700;
+}
+
+.block {
+  background-color: #fff;
+}
+
+h2 {
+  color: #fff;
+  background-color: #42b983;
+  padding: 1em;
+  margin: 0;
+}
+
+form {
+  padding: 1em 1em 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+input {
+  font-size: 1rem;
+  padding: 0.5em;
+  margin-bottom: 0.5em;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+input[type="submit"] {
+  color: #fff;
+  background-color: #42b983;
+  border: 1px solid darken(#42b983, 10%);
+  cursor: pointer;
+
+  &:hover {
+    background-color: darken(#42b983, 10%);
+  }
+}
+
+ul {
+  list-style-type: none;
+  padding: 1em 0;
+  margin: 0;
+}
+
+li {
+  display: block;
+}
+
+a {
+  display: block;
+  color: #42b983;
+  padding: 0.7em 2.2em;
+  font-family: 'Merriweather';
+}
+</style>
